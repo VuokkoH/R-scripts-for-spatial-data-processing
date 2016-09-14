04d9596814389b65fe0b094b0afe9af139b5ad79
Vuokko Heikinheimo <vuokko.heikinheimo@helsinki.fi>
2016-09-14 12:41:02 +0000

    added initial script

--- Visualization/PlotPointsOnAMap_ggmap.R
+++ Visualization/PlotPointsOnAMap_ggmap.R
@@ -1,1 +1,26 @@
-# code here
+# Plot points on a map from selected area with google maps on the bacground 
+# Code based on Milano R blogpost: http://www.milanor.net/blog/maps-in-r-plotting-data-points-on-a-map/ and relevant help-files
+
+# This example is customized for data from Helsinki, Finland
+
+#install packages if necessary
+install.packages("ggmap")
+#load packages
+library(ggmap)
+
+
+# Read in data which has geographic coordinates in stored in columns 'lon' and 'lat'.
+# somedata <- read.csv(filename.csv)
+
+#Create the map object
+#?get_map # run this line for details on the parameters (for example you can try location="Finland)
+map <- get_map(location = 'Helsinki', zoom = 12)
+
+#Plot the bacground map
+#plot(map)  # or just type in 'map' in the console
+ggmap(map)	# here you can add more options for plotting a ggmap object
+
+#Plot points on the map
+mapAndPoints <- ggmap(map) + geom_point(aes(x = lon, y = lat), data = somedata, alpha = .5)
+plot(mapAndPoints) # or just type in "mapAndPoints" in the console
+
